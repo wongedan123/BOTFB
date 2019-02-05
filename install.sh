8cc60897ed47a4e032a4e49be4f02b7393f1830a
Wongedan <47300852+wongedan123@users.noreply.github.com>
2019-02-05 23:10:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,11 @@ pip2 install requests
 sleep 2
 pkg install python
 sleep 2
+pkg install toilet
+sleep 2
+pkg install lolcat
+sleep 2
+gem lolcat
 clear
 echo ""
 echo "INSTALLING FINISH please enter python2 BOTFB.py"
