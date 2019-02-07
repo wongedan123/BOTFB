2c2fd41f7088da5f58f0eef941039346fe9e1d02
Wongedan <47300852+wongedan123@users.noreply.github.com>
2019-02-07 14:10:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,17 +7,17 @@ echo ""
 echo "Pleas wait :) ...."
 echo ""
 sleep 4
-pip2 install mechanize
+pkg pip2 install mechanize
 sleep 2
-pip2 install requests
+pkg pip2 install requests
 sleep 2
-pkg install python
+pkg install python2
 sleep 2
 pkg install toilet
 sleep 2
 pkg install lolcat
 sleep 2
-gem install lolcat
+pkg gem install lolcat
 clear
 echo ""
 echo "INSTALLING FINISH please enter python2 BOTFB.py"
